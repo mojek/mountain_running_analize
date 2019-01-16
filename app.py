ff3ea7bc0475ac4e7b35cfc7cc105c4e38bb10cb
Michal Mojek <m.mojek@gmail.com>
2019-01-16 15:35:00 +0100

    initial layout

---
+++ app.py
@@ -0,0 +1,25 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
+app = dash.Dash(__name__,
+                external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(className="container-fluid",
+                      children=[
+                          html.H1(children='Mountain running analizer')
+                      ])
+
+
+if __name__ == '__main__':
+    app.run_server()
